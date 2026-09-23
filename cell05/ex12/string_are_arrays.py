20cee2461ac551d2c49aa2eb6d7a87beefad5c06
68050075 <68050075@kmitl.ac.th>
2026-09-23 18:56:16 +0700

    submit

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) != 2:
+    print("none")
+else:
+    count = 0
+    for character in sys.argv[1]:
+        if character == "z":
+            print("z", end="")
+            count += 1
+    if count == 0:
+        print("none")
+    else:
+        print()
